8823c4dbdabced262c24627e959b8f60df49b793
ChenJiYuan <447334358@qq.com>
2024-06-05 11:16:36 +0000

    docs: 添加项目外链

--- blog/src/shared/components/header/index.tsx
+++ blog/src/shared/components/header/index.tsx
@@ -10,6 +10,16 @@ const Header: FC = () => {
         <div className={styles['nav-list']}>
           <Link href={'/'}>首页</Link>
           <Link href={'/tag'}>我的笔记</Link>
+          <a
+            href="/use-modal-service"
+            target="_blank"
+            title="更简单的使用antd modal"
+          >useModalService</a>
+          <a
+            href="/bello"
+            target="_blank"
+            title="基于antd的表单差异对比组件"
+          >bello</a>
           <Link href={'/about'}>关于我们</Link>
         </div>
       </nav>
